8f8a778de1534c66edc2acbd64172ef7a15b9f30
Yurii Soroka <yurisoroka165@gmail.com>
2023-01-28 17:35:11 +0200

    28012023

---
+++ js/28012023/dz1.js
@@ -0,0 +1,13 @@
+//1. Напиши скрипт, который для объекта user,
+//последовательно:
+//1 добавит поле mood со значением 'happy'
+//2 заменит hobby на 'skydiving'
+//3 заменит значение premium на false
+//4 выводит содержимое объекта users в фортмате
+//ключ:значение используя Object.keys() и for...of
+const user = {
+  name: "John",
+  age: 20,
+  hobby: "tenis",
+  premium: true,
+};
---
+++ js/28012023/dz6.js
@@ -0,0 +1,12 @@
+//6. Напишите функцию, которая принимает как параметр объект
+//и формирует объекты в новом массиве в формате [key, value]
+// З об"єкта зробити масив масивів типу - [['name': "John"], ['surName': "Stones"], і тд]
+
+const user6 = {
+  name: "John",
+  surName: "Stones",
+  age: 20,
+  hobby: "tenis",
+  haveCar: true,
+  merried: false,
+};
