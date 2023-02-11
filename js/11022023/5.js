b78829b85f5257a33d8f83bd881b2ab6290545d9
Yurii Soroka <yurisoroka165@gmail.com>
2023-02-11 17:04:05 +0200

    update

---
+++ js/11022023/5.js
@@ -0,0 +1,12 @@
+const product = {
+  price: 5000,
+  showPrice() {
+    console.log(this.price);
+  },
+};
+
+function callAction(action) {
+  action();
+}
+
+callAction(product.showPrice.bind(product));
---
+++ js/11022023/6.js
@@ -0,0 +1,18 @@
+//6. Напишите функцию each(array, callback), которая
+//первым параметром принимает массив, а вторым - функцию,
+//которая применится к каждому элементу массива.
+//Функция each должна вернуть новый массив, элементами
+//которого будут результаты вызова callback
+//callback функци должна умножать элементы на 2
+
+const numbers = [3, 5, 6, 34, 8, 83, 12, 34];
+
+function each(array, callback) {
+    return array.map(item => callback(item));
+}
+
+function multiply(element) {
+    return element * 2;
+}
+
+console.log(each(numbers, multiply));
